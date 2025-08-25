61c1bea54d5508cf932696b43bb6f47c89f8c925
Shriballabh Bhatt <shriballabhbhatt1234@gmail.com>
2025-08-25 14:41:05 +0530

    Auth setup and redux store update

--- types/store.ts
+++ types/store.ts
@@ -1,3 +1,5 @@
+import { boolean } from "zod";
+
 export interface Store {
     id: string;
     name: string;
@@ -12,4 +14,49 @@ export interface Billboard {
   storeId: string;
   label: string;
   imageUrl: string;
+  createdAt: string;
+}
+
+export interface Category {
+  id: string;
+  storeId: string;
+  name: string;
+  billboard: Billboard;
+  createdAt: string;
+}
+
+export interface Size {
+  id: number;
+  storeId: string;
+  name: string;
+  value: string;
+  createdAt: string;
+}
+
+export interface Color {
+  id: number;
+  storeId: string;
+  name: string;
+  value: string;
+  createdAt: string;
+}
+
+export interface Image {
+  id: number;
+  url: string;
+  createdAt: string;
+}
+
+export interface Product {
+  id: string;
+  storeId: string;
+  name: string;
+  price: number;
+  isFeatured: boolean;
+  isArchived: boolean;
+  category: Category;
+  size: Size;
+  color: Color;
+  images: Image[];
+  createdAt: string;
 }
